5ccecd89dbf7c9b3d0009be8ded9ba38513386e2
geigerj <jacgeiger@gmail.com>
2020-08-16 16:03:50 -0400

    explicitly reset perspective to fix blank background issue

--- background.js
+++ background.js
@@ -23,6 +23,11 @@ function draw() {
     canvasResized = true;
   }
   
+  // Reset perspective. This shouldn't be necessary but for unknown reasons,
+  // the perspective sometimes gets set to invalid near/far distances, causing
+  // a blank background (can't reproduce consistently)
+  perspective();
+  
   shader(bgShader);
   bgShader.setUniform('time', frameCount);
   rect(0,0,width, height);
